3ab000e73daa02943b24f57f4c67dc81b8b9d4c5
Anand Kumar <anandkumar@TD-0010.local>
2019-09-04 12:10:49 -0700

    first commit

---
+++ src/Components/ProfileDialog/index.js
@@ -0,0 +1,25 @@
+import InterestWordsCard from "./InterestWords/InterestWordsCard";
+import JourneyCard from "./Journey/JourneyCard";
+import SideBar from "./SideBar/SideBar";
+import PreferencesCard from "./Preferences/PreferencesCard";
+import PsychographicsCard from "./Psychographics/PsychographicsCard";
+import RevenueCard from "./Revenue/RevenueCard";
+import ScoresCard from "./Scores/ScoresCard";
+import SegmentsCard from "./Segments/SegmentsCard";
+import SourcesCard from "./Sources/SourcesCard";
+import ImageCard from "./Images/ImageCard";
+import PaintCard from "./Paint/PaintCard";
+
+export {
+  InterestWordsCard,
+  JourneyCard,
+  SideBar,
+  PreferencesCard,
+  PsychographicsCard,
+  RevenueCard,
+  ScoresCard,
+  SegmentsCard,
+  SourcesCard,
+  ImageCard,
+  PaintCard
+};
